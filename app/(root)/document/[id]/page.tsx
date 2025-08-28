8205ef107d1af700ac60537ac0acd7bbbe14c9ac
frosthowl <rohandhanerwal@gmail.com>
2025-08-28 22:37:27 +0530

    commit

---
+++ app/(root)/document/[id]/page.tsx
@@ -0,0 +1,12 @@
+import { Editor } from '@/components/components/editor/Editor'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Editor/>
+    </div>
+  )
+}
+
+export default page
---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+const config = {
+  darkMode: 'class', // Enable dark mode via .dark class
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
